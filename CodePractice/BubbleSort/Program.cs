a052ae755fc8030c6d701a92749dcd2bdba7ca32
W.M Cheng <jhengsiaomin@gmail.com>
2020-03-07 15:54:10 +0800

    增加選擇排序跟氣泡排序

---
+++ CodePractice/BubbleSort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BubbleSort
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var testData = new int[] { 72, 25, 7, 83, 93, 4, 11, 6, 10, 15, 28, 3, 77, 63, 98, 91 };
+
+            for (var i = 0; i < testData.Length - 1; i++)
+            {
+                for (var j = 0; j < testData.Length - i - 1; j++)
+                {
+                    if (testData[j] > testData[j + 1])
+                    {
+                        var temp = testData[j];
+                        testData[j] = testData[j + 1];
+                        testData[j + 1] = temp;
+                    }
+                }
+            }
+
+            var result = string.Join(",", testData);
+            Console.Write(result);
+            Console.Read();
+        }
+    }
+}
---
+++ CodePractice/SelectSort/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SelectSort
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var testData = new int[] { 5, 3, 8, 1, 4, 7, 2, 9, 6 };
+            var minIndex = 0;
+            for (var i = 0; i < testData.Length - 1; i++)
+            {
+                minIndex = i;
+
+                for (var j = i + 1; j < testData.Length; j++)
+                {
+                    if (testData[minIndex] > testData[j])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                var temp = testData[i];
+                testData[i] = testData[minIndex];
+                testData[minIndex] = temp;
+            }
+
+            var result = string.Join(",", testData);
+
+            Console.WriteLine(result);
+
+            //暫停畫面
+            Console.Read();
+        }
+    }
+}
